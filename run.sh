9a5f801ee2eb04c2dc318398694e15ba5f9f5f65
bagushandhoko <58882667+bagushandhoko@users.noreply.github.com>
2025-10-15 19:36:20 +0700

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv bashio
-bashio::log.info "Starting Comfort Entity Cleanup..."
-
-# Jalankan Python script utama
-python3 /app/cleanup.py
-
-bashio::log.info "Comfort Entity Cleanup finished."
